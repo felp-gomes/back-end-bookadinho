e2c6d40a30cff252662009379e61b259c0b2464f
paulacynthia <pcynthia013@gmail.com>
2022-12-05 21:35:31 -0300

    feat: update photos and indentation

--- src/mocks/books.js
+++ src/mocks/books.js
@@ -1,78 +1,97 @@
 const books = [
   {
-    "id": "0001",
-    "name": "Neuromancer",
-    "author": "William Gibson",
-    "description":
+    id: "0001",
+    name: "Neuromancer",
+    author: "William Gibson",
+    description:
       "The sky over the harbor was the color of television tuned to an offline channel.",
-    "photo": "https://i.zst.com.br/thumbs/12/5/c/1458194278.jpg",
-    "isChange": "false",
-    "profile": {
-      "id": "0001",
-      "userName": "felipe.ogoms",
+    photo: "/assets/neuromancer.png",
+    isChange: "false",
+    profile: {
+      id: "0001",
+      userName: "felipe.ogoms",
     },
   },
-
   {
-    "id": "0002",
-    "name": "Jogador N1",
-    "author": "Ernest Cline",
-    "description":
+    id: "0002",
+    name: "Jogador N1",
+    author: "Ernest Cline",
+    description:
       "Eu tinha visto primeiro o filme e tinha gostado bastante. Não sabia que existia o livro, mas quando eu li. Me apaixonei na mesma hora, foi incrível. É como se eu estivesse sentindo as mesmas emoções do personagem. Nunca fiquei tão animado para ler um livro.",
-    "photo":
-      "https://m.media-amazon.com/images/I/91z865cb5-L._AC_SY879_.jpg",
-    "isChange": "false",
-    "profile": {
-      "id": "0001",
-      "userName": "felipe.ogoms",
+    photo: "/assets/jogadorN1.jpg",
+    isChange: "false",
+    profile: {
+      id: "0001",
+      userName: "felipe.ogoms",
     },
   },
-
   {
-    "id": "0003",
-    "name": "Tartarugas até lá em baixo",
-    "author": "John Green",
-    "description":
+    id: "0003",
+    name: "Tartarugas até lá em baixo",
+    author: "John Green",
+    description:
       "Um livro excelente para entender as nossas mentes e ações. E ter mais empatia sobre o que se pode passar na cabeça do coleguinha :)",
-    "photo": "",
-    "isChange": "true",
-    "profile": {
-      "id": "0001",
-      "userName": "felipe.ogoms",
+    photo: "/assets/turtles.jpg",
+    isChange: "true",
+    profile: {
+      id: "0001",
+      userName: "felipe.ogoms",
     },
   },
-
   {
-    "id": "0004",
-    "name": "Clean code",
-    "author": "Robert Martin",
-    "description":
+    id: "0004",
+    name: "Clean Code",
+    author: "Robert Martin",
+    description:
       "O livro que me fez me sobre sair no estágio que trabalhei, era o único que tinha conhecimento sobre o clean code e padrões de projeto. Gostaria que outra pessoa também tivesse essa experiência.",
-    "photo":
-      "https://miro.medium.com/max/419/1*mSOd9nC_p3fP6cDPUSluAw.jpeg",
-    "isChange": "false",
-    "profile": {
-      "id": "0002",
-      "userName": "guaranolas",
+    photo: "/assets/cleancode.jpg",
+    isChange: "false",
+    profile: {
+      id: "0002",
+      userName: "guaranolas",
     },
   },
-
   {
-    "id": "0005",
-    "name": "As extraordinárias cores do amanhã",
-    "author": "Emily X.R. Pan",
-    "description":
+    id: "0005",
+    name: "As Extraordinárias Cores do Amanhã",
+    author: "Emily X.R. Pan",
+    description:
       "Extremamente intrigante do começo ao fim. Com uma construção de personagem esplêndida, você fica frisado e quer terminar o mais breve possível. Sem dúvidas uma das melhores leituras de 2022.",
-    "photo":
-      "https://miro.medium.com/max/419/1*mSOd9nC_p3fP6cDPUSluAw.jpeg",
-    "isChange": "false",
-    "profile": {
-      "id": "0004",
-      "userName": "paulacynthia",
+    photo: "/assets/asExtraodinariasCoresDoAmanha.png",
+    isChange: "false",
+    profile: {
+      id: "0004",
+      userName: "paulacynthia",
+    },
+  },
+  {
+    id: "0006",
+    name: "Rainha Vermelha",
+    author: "Victoria Aveyard",
+    description:
+      "No início eu estava esperando um roteiro óbvio, mas depois que li o livro inteiro, me apaixonei! A história é muito boa, a escrita é melhor ainda, recomendo muito!!",
+    photo: "/assets/rainhaVermelha.jpg",
+    isChange: "false",
+    profile: {
+      id: "0004",
+      userName: "paulacynthia",
+    },
+  },
+  {
+    id: "0007",
+    name: "Harry Potter e a Pedra Filosofal",
+    author: "JK Rowling",
+    description:
+      "Harry Potter e a Pedra Filosofal foi um livro que me tirou da monotonia e me levou para um mundo esplêndido. Hoje, com cerca de 3 leituras acredito que ele mereça encontrar um novo potterhead!",
+    photo: "/assets/harryPotter.png",
+    isChange: "false",
+    profile: {
+      id: "0004",
+      userName: "paulacynthia",
     },
   },
 ];
 
 module.exports = {
-  books
-}
+  books,
+};
--- src/mocks/profiles.js
+++ src/mocks/profiles.js
@@ -1,11 +1,11 @@
 const profiles = [
   {
-    "id": "0001",
-    "name": "Felipe Gomes",
-    "userName": "felipe.ogoms",
-    "description":
+    id: "0001",
+    name: "Felipe Gomes",
+    userName: "felipe.ogoms",
+    description:
       "The sky over the harbor was the color of television tuned to an offline channel.",
-    "likes": [
+    likes: [
       "LoL",
       "Ficção",
       "Romance",
@@ -14,89 +14,85 @@ const profiles = [
       "Tecnologia",
       "Fanfic",
     ],
-    "changeBooks": [
+    changeBooks: [
       {
-        "id": "0001",
-        "name": "Neuromancer",
-        "photo": "https://i.zst.com.br/thumbs/12/5/c/1458194278.jpg"
+        id: "0001",
+        name: "Neuromancer",
+        photo: "https://i.zst.com.br/thumbs/12/5/c/1458194278.jpg",
       },
       {
-        "id": "0002",
-        "name": "Jogador N1",
-        "photo": "https://m.media-amazon.com/images/I/91z865cb5-L._AC_SY879_.jpg"
+        id: "0002",
+        name: "Jogador N1",
+        photo: "https://m.media-amazon.com/images/I/91z865cb5-L._AC_SY879_.jpg",
       },
       {
-        "id": "0003",
-        "name": "Tartarugas até lá em baixo",
-        "photo": ""
-      }
+        id: "0003",
+        name: "Tartarugas até lá em baixo",
+        photo: "",
+      },
     ],
-    "latestReadings": [
+    latestReadings: [
       "Jogador N1",
       "Tartarugas até lá em baixo",
       "Alice no pais das maravilhas",
       "Até a próxima estrela",
     ],
-    "photo":
+    photo:
       "https://instagram.ffor29-1.fna.fbcdn.net/v/t51.2885-19/151841773_141305464507911_2460237208396643498_n.jpg?stp=dst-jpg_s150x150",
-    "password": "senha" 
+    password: "senha",
   },
-
   {
-    "id": "0002",
-    "name": "Guarany",
-    "userName": "guaranolas",
-    "description": "Programador e tranquilo com a vida",
-    "likes": ["Computador", "Jogos", "Animes", "Séries"],
-    "changeBooks": [
+    id: "0002",
+    name: "Guarany",
+    userName: "guaranolas",
+    description: "Programador e tranquilo com a vida",
+    likes: ["Computador", "Jogos", "Animes", "Séries"],
+    changeBooks: [
       {
-        "id": "0004",
-        "name": "Clean code",
-        "photo": "https://miro.medium.com/max/419/1*mSOd9nC_p3fP6cDPUSluAw.jpeg"
-      }
+        id: "0004",
+        name: "Clean code",
+        photo: "https://miro.medium.com/max/419/1*mSOd9nC_p3fP6cDPUSluAw.jpeg",
+      },
     ],
-    "latestReadings": ["Clean code", "Algoritmos na prática"],
-    "photo": "https://thispersondoesnotexist.com/image",
-    "password": "1234"
+    latestReadings: ["Clean code", "Algoritmos na prática"],
+    photo: "https://thispersondoesnotexist.com/image",
+    password: "1234",
   },
-
   {
-    "id": "0003",
-    "name": "Carlos",
-    "userName": "case",
-    "description":
+    id: "0003",
+    name: "Carlos",
+    userName: "case",
+    description:
       "Apaixonado por livros e arquitetura. 📷 Gosto de tirar fotos em prédios antigos e abandonas. Amo ver a paisagem do urbana do topo de um prédio!",
-    "likes": ["Fotografia", "Livros", "Urbanismo", "Animes"],
-    "changeBooks": [],
-    "latestReadings": ["A arte de projetar em arquitetura"],
-    "photo": "",
-    "password": "password"
+    likes: ["Fotografia", "Livros", "Urbanismo", "Animes"],
+    changeBooks: [],
+    latestReadings: ["A arte de projetar em arquitetura"],
+    photo: "",
+    password: "password",
   },
-
   {
-    "id": "0004",
-    "name": "Cynthia",
-    "userName": "paulacynthia",
-    "description": "Desenvolvedora front-end e leitora nas horas vagas",
-    "likes": ["Animes", "Livros", "Música", "Tecnologia"],
-    "changeBooks": [
+    id: "0004",
+    name: "Cynthia",
+    userName: "paulacynthia",
+    description: "Desenvolvedora front-end e leitora nas horas vagas",
+    likes: ["Animes", "Livros", "Música", "Tecnologia"],
+    changeBooks: [
       {
-        "id": "0005",
-        "name": "As extraordinárias cores do amanhã",
-        "photo": "https://miro.medium.com/max/419/1*mSOd9nC_p3fP6cDPUSluAw.jpeg"
-      }
+        id: "0005",
+        name: "As extraordinárias cores do amanhã",
+        photo: "https://miro.medium.com/max/419/1*mSOd9nC_p3fP6cDPUSluAw.jpeg",
+      },
     ],
-    "latestReadings": [
+    latestReadings: [
       "As extraordinárias cores do amanhã",
       "Rainha Vermelha",
       "Até a próxima estrela",
     ],
-    "photo": "",
-    "password": "pequena"
+    photo: "",
+    password: "pequena",
   },
 ];
 
-
 module.exports = {
-  profiles
-}
+  profiles,
+};
