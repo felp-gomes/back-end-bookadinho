010890bb05c1337af3dee3241f042745c6776ff1
STL-FelipeGomes <felipegomesoliveira12@gmail.com>
2023-04-03 21:40:16 -0300

    :art: environment variable improvement

--- src/models/OAuth.ts
+++ src/models/OAuth.ts
@@ -2,9 +2,13 @@ import jwt from 'jsonwebtoken';
 class WebToken {
   static createToken(user: { id: string; user_name: string }) {
     try {
-      const token: unknown = jwt.sign(user, process.env.SECRET_KEY_JWT!, {
-        algorithm: 'HS256',
-      }) as jwt.SignOptions;
+      const token: unknown = jwt.sign(
+        user,
+        process.env.SECRET_KEY_JWT as string,
+        {
+          algorithm: 'HS256',
+        }
+      ) as jwt.SignOptions;
       if (typeof token !== 'string') {
         throw 'REQUEST TOKEN DIFFERENT FROM STRING';
       }
