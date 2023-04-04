bcf109a93bb529d14ee5eb4232d9a8b40da8c5d8
STL-FelipeGomes <felipegomesoliveira12@gmail.com>
2023-04-03 22:56:49 -0300

    :zap: added profile query to token request

--- src/controllers/profileController.ts
+++ src/controllers/profileController.ts
@@ -4,24 +4,26 @@ import OAuth from '../models/OAuth';
 
 export default class ProfileController {
   static authenticateProfile(req: express.Request, res: express.Response) {
-    const { user_name, password }: { user_name: string; password: string } =
-      req.body;
+    const { user_name, password }: { user_name: string; password: string } = req.body;
     if (!user_name || !password) {
       return res.status(401).send({
         status: 400,
-        message: 'User name and password profile id are required!',
+        body: {
+          message: 'User name and password profile id are required!',
+        },
       });
     }
 
     const foundProfileIndex = profiles.findIndex(
-      (profile) =>
-        profile.user_name === user_name && profile.password === password
+      (profile) => profile.user_name === user_name && profile.password === password
     );
 
     if (foundProfileIndex === -1) {
       return res.status(401).send({
         status: 401,
-        message: 'Incorrect username or password!',
+        body: {
+          message: 'Incorrect username or password!',
+        },
       });
     }
 
@@ -32,15 +34,57 @@ export default class ProfileController {
       });
 
       profiles[foundProfileIndex].authorizations.push(tokenByProfile);
-      return res
-        .status(202)
-        .send({ status: 202, body: { authorization: tokenByProfile } });
+      return res.status(202).send({ status: 202, body: { authorization: tokenByProfile } });
     } catch (error) {
       res.status(400).send({ status: 400, message: error });
     }
   }
 
   static listProfiles(req: express.Request, res: express.Response) {
-    return res.status(200).json({ status: 200, message: profiles });
+    const authorizationProfile = req.headers.authorization as string | undefined;
+
+    if (!authorizationProfile) {
+      return res.status(400).send({
+        status: 401,
+        body: {
+          message: 'authorizationProfile is required!',
+        },
+      });
+    }
+
+    try {
+      const decryptedToken = OAuth.verifyToken(authorizationProfile) as {
+        id: string;
+        user_name: string;
+      };
+
+      const foundProfileByToken = profiles.find(
+        (profile) => profile.id === decryptedToken.id && profile.user_name == decryptedToken.user_name
+      );
+
+      if (!foundProfileByToken) {
+        return res.status(401).send({
+          status: 401,
+          body: { message: 'Profile not found!' },
+        });
+      }
+
+      const { authorizations: tokensProfile }: { authorizations: string[] } = foundProfileByToken;
+
+      const isValidToken = tokensProfile.some((token) => token == authorizationProfile);
+
+      if (!isValidToken) {
+        return res.status(401).send({
+          status: 401,
+          body: {
+            message: 'The token is not valid to perform operations!',
+          },
+        });
+      }
+
+      return res.status(200).json({ status: 200, body: { message: 'ok', profiles: profiles } });
+    } catch (error) {
+      res.status(400).send({ status: 400, body: { message: error } });
+    }
   }
 }
