0aa4e6af4300b68b5445bf56efd811f25441d980
STL-FelipeGomes <felipegomesoliveira12@gmail.com>
2023-04-04 23:14:08 -0300

    :art: going back to the original code

--- src/interfaces/profile.ts
+++ src/interfaces/profile.ts
@@ -2,15 +2,15 @@ export default interface Profile {
   id: string;
   user_name: string;
   name: string;
-  description?: string;
-  likes?: string[];
-  change_books?: {
+  description: string;
+  likes: string[];
+  change_books: {
     id: string;
     name: string;
     photo: string;
   }[];
-  latest_readings?: string[];
-  photo?: string;
+  latest_readings: string[];
+  photo: string;
   password: string;
-  authorizations?: string[];
+  authorizations: string[];
 }
