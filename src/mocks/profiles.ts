da1c0272ae9f4f3e9be0e3af143ff8cbc7289438
STL-FelipeGomes <felipegomesoliveira12@gmail.com>
2023-04-07 18:16:46 -0300

    :clown_face: add token in profile

--- src/mocks/profiles.ts
+++ src/mocks/profiles.ts
@@ -33,7 +33,9 @@ const profiles: ProfileInterface[] = [
     photo:
       'https://instagram.ffor29-1.fna.fbcdn.net/v/t51.2885-19/151841773_141305464507911_2460237208396643498_n.jpg?stp=dst-jpg_s150x150',
     password: 'senha',
-    authorizations: [],
+    authorizations: [
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjAwMDEiLCJ1c2VyX25hbWUiOiJmZWxpcGUub2dvbXMiLCJpYXQiOjE2ODA4OTA3ODN9.oVKj9p86GK7OYg4OTCpKeuloa_A4gcIgjtAdRkXn2P4',
+    ],
   },
   {
     id: '0002',
@@ -82,7 +84,9 @@ const profiles: ProfileInterface[] = [
     latest_readings: ['As extraordinárias cores do amanhã', 'Rainha Vermelha', 'Até a próxima estrela'],
     photo: '',
     password: 'pc',
-    authorizations: [],
+    authorizations: [
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjAwMDQiLCJ1c2VyX25hbWUiOiJwYXVsYWN5bnRoaWEiLCJpYXQiOjE2ODA4OTA4MTh9.51hI5jJj2LOkB4kotF8C5MtLPJpTexVqEJza6OlIvMM',
+    ],
   },
 ];
 
