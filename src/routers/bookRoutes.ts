c3a7ec8621272f89e01c4aca9b466a071c914d68
Felipe Gomes <felipegomesoliveira12@gmail.com>
2023-06-24 11:12:07 -0300

    :sparkles: modify router the books

--- src/routers/bookRoutes.ts
+++ src/routers/bookRoutes.ts
@@ -3,6 +3,6 @@ import BookController from '../controllers/BookController';
 
 const router = Router();
 
-router.get('/book', BookController.listBooks).get('/book/:id', BookController.listBookById);
+router.get('/books', BookController.listBooks).get('/book/:id', BookController.listBookById);
 
 export default router;
