7fb5f57be0af57cfb85d5b1a25e3beb919d6326f
STL-FelipeGomes <felipegomesoliveira12@gmail.com>
2023-08-19 14:03:48 -0300

    :rocket: last commit

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import app from './app';
 
-const port: string | number = process.env.PORT || 3000;
+const port = 3000;
 
 app.listen(port, () => {
   console.log(`\x1b[32m[Servidor up http://localhost:${port}]\x1b[0m`);
