08c8913832874902b3214a607558a621f45bcedc
STL-FelipeGomes <felipegomesoliveira12@gmail.com>
2023-08-27 20:22:31 -0300

    :egg: change token

--- src/mocks/authorizations.ts
+++ src/mocks/authorizations.ts
@@ -1,8 +1,8 @@
 import AuthorizationInterface from '../interfaces/authorization.js';
 
 const authorizations: AuthorizationInterface = {
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjAwMDEiLCJ1c2VyX25hbWUiOiJmZWxpcGUub2dvbXMiLCJpYXQiOjE2ODA4OTA3ODN9.oVKj9p86GK7OYg4OTCpKeuloa_A4gcIgjtAdRkXn2P4':
-    '0001',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImJhNzk0M2I2LWMxNWYtNDNjNi1iNGZhLTk2MDBmYmM1MGU0MCIsImlhdCI6MTY5MzE3MzcxOX0.oJSG_eTLIvxEp5tBNmBOBROpQFQKNQMhRcKjs-pIwxw':
+    'ba7943b6-c15f-43c6-b4fa-9600fbc50e40',
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjAwMDQiLCJ1c2VyX25hbWUiOiJwYXVsYWN5bnRoaWEiLCJpYXQiOjE2ODA4OTA4MTh9.51hI5jJj2LOkB4kotF8C5MtLPJpTexVqEJza6OlIvMM':
     '004',
 };
