7984af2f8edcfecd747b348b247d97c379ef1021
STL-FelipeGomes <felipegomesoliveira12@gmail.com>
2023-08-26 21:26:14 -0300

    :zap: preventing loss of works

--- src/bin/www/server.ts
+++ src/bin/www/server.ts
@@ -5,11 +5,25 @@ import app from '../../app.js';
 const cpus = os.cpus();
 const port = 3000;
 
+const onWorkerError = (code: unknown, signal: unknown) => {
+  console.log(code, signal);
+};
+
 if (cluster.isPrimary) {
-  cpus.forEach(() => cluster.fork());
-  cluster.on('exit', (error) => console.debug(error));
+  cpus.forEach(() => {
+    const worker = cluster.fork();
+    worker.on('error', onWorkerError);
+  });
+
+  cluster.on('exit', (error) => {
+    const newWorker = cluster.fork();
+    newWorker.on('error', onWorkerError);
+    console.debug('\x1b[33m[WORKER PROCESS EXITED]\x1b[0m', newWorker.process.pid);
+    console.debug(error);
+  });
 } else {
-  app.listen(port, () => {
+  const server = app.listen(port, () => {
     console.debug(`\x1b[32m[Servidor up http://localhost:${port}]\x1b[0m`);
   });
+  server.on('error', (error) => console.debug(error));
 }
