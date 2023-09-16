cfc8e210d5780f7c71d4f65682284c2c2e6f8fa3
STL-FelipeGomes <felipegomesoliveira12@gmail.com>
2023-09-15 23:17:59 -0300

    :sparkles: adapted editProfile consultation on the prisma

--- src/controllers/ProfileController.ts
+++ src/controllers/ProfileController.ts
@@ -217,90 +217,86 @@ export default class ProfileController {
       });
     }
   }
-  public static editProfile(req: Request, res: Response) {
-    const foundProfileByToken: UserType = res.locals.foundProfileByToken;
-    const { user_name, name, description, likes, latest_readings, photo, password, email }: UserType = req.body;
-    if (user_name !== foundProfileByToken.user_name && profiles.some((profile) => profile.user_name === user_name)) {
-      return res.status(409).send({
-        body: {
-          status_code: 409,
-          status: 'fail',
-          message: 'Username is already in use!',
+  public static async editProfile(req: Request, res: Response) {
+    const authenticatedUser: UserType = res.locals.foundProfileByToken;
+    const {
+      user_name,
+      name,
+      description = null,
+      likes = [],
+      latest_readings = [],
+      photo = null,
+      password,
+      email,
+    }: UserType = req.body;
+
+    try {
+      const isUserAlready = await prismaUsers.findFirst({
+        where: {
+          OR: [
+            {
+              user_name: {
+                contains: user_name,
+              },
+            },
+            {
+              email: {
+                contains: email,
+              },
+            },
+          ],
         },
       });
-    }
-    if (user_name && (user_name.length < 4 || user_name.length > 20)) {
-      return res.status(403).send({
-        body: {
-          status_code: 403,
-          status: 'fail',
-          message: 'Username must be between 4 and 20 characters!',
-        },
-      });
-    }
-    if (name && (name.length < 4 || name.length > 45)) {
-      return res.status(403).send({
-        body: {
-          status_code: 403,
-          status: 'fail',
-          message: 'Name must be between 4 and 45 characters!',
-        },
-      });
-    }
-    if (email && !validateProfileEmail(email)) {
-      return res.status(403).send({
-        body: {
-          status_code: 403,
-          status: 'fail',
-          message: 'Not a valid email!',
-        },
-      });
-    }
-    if (password && (password.length < 6 || password.length > 45)) {
-      return res.status(403).send({
-        body: {
-          status_code: 403,
-          status: 'fail',
-          message: 'Password must be between 6 and 45 characters!',
-        },
-      });
-    }
-    if (description && (description.length < 1 || description.length > 250)) {
-      return res.status(413).send({
-        body: {
-          status_code: 413,
-          status: 'fail',
-          message: 'Description is limited to 250 characters!',
-        },
-      });
+      if (isUserAlready)
+        return res.status(409).send({
+          body: {
+            status_code: 409,
+            status: 'fail',
+            message: 'Username or email is already in use!',
+          },
+        });
+    } catch (error) {
+      return res.status(500).send({ body: { status_code: 500, status: 'fail', message: 'Internal error!' } });
     }
 
-    const updatedProfile: UserType = {
-      ...foundProfileByToken,
-      user_name: user_name ? user_name.trim() : foundProfileByToken.user_name,
-      name: name ? name.trim() : foundProfileByToken.name,
-      description: description ? description.trim() : foundProfileByToken.description,
-      likes: likes ? likes.map((like) => like) : foundProfileByToken.likes,
-      latest_readings: latest_readings
-        ? latest_readings.map((latestReadings) => latestReadings)
-        : foundProfileByToken.latest_readings,
-      photo: photo ? photo.trim() : foundProfileByToken.photo,
-      password: password ? password.trim() : foundProfileByToken.password,
-      email: email ? email.trim() : foundProfileByToken.email,
-    };
-    const foundProfileIndex = profiles.findIndex(({ id }) => id === updatedProfile.id);
-    for (const authorization in authorizations) {
-      if (password && authorizations[authorization] === updatedProfile.id) {
-        delete authorizations[authorization];
+    try {
+      const validateDataUser = UserValidation.safeParse({
+        id: authenticatedUser.id,
+        email: email ?? authenticatedUser.email,
+        user_name: user_name ?? authenticatedUser.user_name,
+        name: name ?? authenticatedUser.name,
+        description: description ?? authenticatedUser.description,
+        likes: likes.length === 0 ? authenticatedUser.likes : likes,
+        latest_readings: latest_readings.length === 0 ? authenticatedUser.latest_readings : latest_readings,
+        photo: photo ?? authenticatedUser.photo,
+        password: password ?? authenticatedUser.password,
+        is_activated: true,
+      });
+      if (!validateDataUser.success) {
+        throw validateDataUser.error;
       }
+      const updatedUser = await prismaUsers.update({
+        where: {
+          id: authenticatedUser.id,
+        },
+        data: {
+          ...validateDataUser.data,
+        },
+      });
+      if (password) {
+        await prismaAuthorizations.deleteMany({
+          where: {
+            user_id: authenticatedUser.id,
+          },
+        });
+        return res
+          .status(202)
+          .send({ body: { status_code: 202, status: 'success', message: 'Profile changed successfully!' } });
+      }
+      return res.status(202).send({ body: { status_code: 202, status: 'success', profile: updatedUser } });
+    } catch (error) {
+      console.debug('error', JSON.stringify(error, null, 2));
     }
-    profiles[foundProfileIndex] = updatedProfile;
-    if (password) {
-      return res
-        .status(202)
-        .send({ body: { status_code: 202, status: 'success', message: 'Profile changed successfully!' } });
-    }
-    return res.status(202).send({ body: { status_code: 202, status: 'success', profile: updatedProfile } });
   }
   public static deleteProfile(req: Request, res: Response) {
     const foundProfileByToken: UserType = res.locals.foundProfileByToken;
