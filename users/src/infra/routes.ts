e05a7efe7c646b5a17fdd3506857b0dfc353ea54
STL-FelipeGomes <felipegomesoliveira12@gmail.com>
2023-10-25 23:51:49 -0300

    :sparkles: add exception routes

--- users/src/infra/routes.ts
+++ users/src/infra/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { UserController } from '../modules/users/user.controller.js';
 import { Auth } from '../middleware/auth.provider.js';
+import { ExceptionRoutesController } from '../modules/errors/exceptionRoutes.controller.js';
 
 const routes = Router();
 
@@ -14,6 +15,7 @@ routes
   .post('/users/auth', userController.loginUser.bind(userController))
   .put('/users/:id', auth.verifyAuthentication.bind(auth), userController.updateUser.bind(userController))
   .delete('/users/:id', auth.verifyAuthentication.bind(auth), userController.deleteUser.bind(userController))
-  .delete('/users/logout/:id', auth.verifyAuthentication.bind(auth), userController.logoutUser.bind(userController));
+  .delete('/users/logout/:id', auth.verifyAuthentication.bind(auth), userController.logoutUser.bind(userController))
+  .get('/', new ExceptionRoutesController().routeRoot);
 
 export default routes;
